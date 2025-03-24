bf28d806a7aadcada370d3024150af61799b9fc7
nik <muchknowledgetho@gmail.com>
2025-03-24 12:45:57 +0100

    dependencies upd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,46 +1,90 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.parcelize)
+    alias(libs.plugins.kapt)
+    id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.example.interactiveventurestechtask"
-    compileSdk = 34
+    namespace = "com.example.tapYouTT"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.interactiveventurestechtask"
+        applicationId = "com.example.tapYouTT"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    flavorDimensions += "environment"
+
+    productFlavors {
+        create("dev") {
+
+            buildConfigField("String", "BASE_URL", "\"https://hr-challenge.dev.tapyou.com/\"")
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 }
 
 dependencies {
 
+    // Base
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.constraintlayout)
+
+    // Kotlin
+    implementation(libs.kotlin.stdlib)
+
+    // Moxy
+    implementation(libs.moxy.core)
+    implementation(libs.moxy.ktx)
+    implementation(libs.moxy.android)
+    implementation(libs.moxy.androidx)
+    implementation(libs.moxy.compiler)
+
+    // Retrofit
+    implementation(libs.gson)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson.converter)
+    implementation(libs.retrofit.rxjava.adapter)
+    implementation(libs.retrofit.int)
+
+    // Rx
+    implementation(libs.rx.android)
+    implementation(libs.rx.java)
+
+    // Cicerone
+    implementation(libs.cicerone)
+
+    // Timber
+    implementation(libs.timber)
+
+    // Dagger
+    implementation(libs.dagger.core)
+    implementation(libs.dagger.android)
+    implementation(libs.dagger.android.support)
+    kapt(libs.dagger.compiler)
+    kapt(libs.dagger.android.processor)
+}
+
+kapt {
+    correctErrorTypes = true
 }
